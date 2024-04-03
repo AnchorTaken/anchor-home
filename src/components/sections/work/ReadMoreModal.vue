f44bba0c84cffa2071b9c99cbe017a90d7644be0
anchor <evalds655@gmail.com>
2024-04-03 20:09:26 +0100

    Added icons. Fixed scroll

--- src/components/sections/work/ReadMoreModal.vue
+++ src/components/sections/work/ReadMoreModal.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="fixed top-0 left-0 z-30 flex h-full w-full flex-col   bg-white p-8 pb-14 backdrop-blur space-y-2 lg:bg-opacity-15 lg:pb-0">
+  <div class="fixed top-0 left-0 z-30 flex h-full w-full flex-col   bg-white p-5 pl-6 pb-14 backdrop-blur space-y-2 lg:bg-opacity-15 lg:pb-0">
     <div class="relative z-10 m-auto rounded-xl bg-white space-y-4 animate__animated animate__fadeIn animate__faster lg:w-2/3 lg:p-10">
       <h2 class="break-all font-black uppercase font-barlow text-[2rem] leading-[3rem]">
         {{ title }}
       </h2>
-      <div class="w-full py-4 space-y-2 ">
-        <p class="whitespace-pre-line text-sm pr-3 font-semibold uppercase text-black font-jet lg:max-h-[40vh] lg:overflow-y-auto" v-html="addBreaks(content)"/>
+      <div class="w-full py-4 space-y-2 flex ">
+        <p class="whitespace-pre-line text-sm pr-2 font-semibold uppercase text-black font-jet lg:max-h-[40vh] overflow-y-scroll max-h-[70vh] " v-html="addBreaks(content)"/>
       </div>
       <button class="bottom-0 left-0 z-30 hidden w-fit mx-auto bg-white px-4 font-black uppercase font-jet lg:relative lg:flex" @click="$emit('close')">
         close
